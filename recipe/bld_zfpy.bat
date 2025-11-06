e08b30f22bb7c127804024ecc88e5ae5eea15f77
Andrii-Osipov <aosipov@anaconda.com>
2025-11-06 04:11:57 -0800

    zfpy 1.0.1

--- recipe/bld_zfpy.bat
+++ recipe/bld_zfpy.bat
@@ -8,13 +8,7 @@ cd build
 
 set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,2%.lib
 
-:: hmaarrfk: 2020/06/20
-:: Basically, this build is going to reinstall the C libraries
-:: we already compiled before
-:: but since the build is identical, conda will not find the newly compiled
-:: libraries, and just keep using the old ons
-:: Configure using the CMakeFiles
-cmake -G "Visual Studio 16 2019"               ^
+cmake -G "Ninja"                               ^
   -DBUILD_ZFPY=ON                              ^
   -DBUILD_UTILITIES=ON                         ^
   -DBUILD_CFP=ON                               ^
@@ -24,14 +18,19 @@ cmake -G "Visual Studio 16 2019"               ^
   -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
   -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"    ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%         ^
+  -DPython_ROOT_DIR=%PREFIX%                   ^
+  -DPython_FIND_VIRTUALENV=ONLY                ^
   ..
 
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
+if errorlevel 1 exit 1
 
-COPY "%SRC_DIR%\build\bin\Release\*.pyd" "%PREFIX%\DLLs"
-COPY "%SRC_DIR%\build\lib\Release\*" "%LIBRARY_LIB%"
-COPY "%SRC_DIR%\build\bin\Release\*" "%LIBRARY_BIN%"
+:: Ninja outputs to bin/, lib/ directly (no Release/ subdirectory)
+COPY "%SRC_DIR%\build\bin\*.pyd" "%PREFIX%\DLLs"
+if errorlevel 1 exit 1
+COPY "%SRC_DIR%\build\lib\*" "%LIBRARY_LIB%"
+if errorlevel 1 exit 1
+COPY "%SRC_DIR%\build\bin\*" "%LIBRARY_BIN%"
 if errorlevel 1 exit 1
